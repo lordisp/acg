bf8ccedd322243e2f327eebf11c7c93540988f6e
Rafael Camison <adm_rafael.camison@lufthansagroup.onmicrosoft.com>
2022-01-10 01:05:51 +0100

    chapter 2.3

--- azure/powershell/3.2-provition-a-cosmos-db-instance/deploy.ps1
+++ azure/powershell/3.2-provition-a-cosmos-db-instance/deploy.ps1
@@ -1,3 +1,18 @@
 $require = $PSScriptRoot + "\..\require\login.ps1";
 . $require
 
+$ResourceGroup = Get-AzResourceGroup
+$template = $PSScriptRoot + '\template\azuredeploy.json'
+$parameters = $PSScriptRoot + '\template\parameters.json'
+
+if ($null -eq $deploymentName)
+{
+    $suffix = Get-Random -Maximum 1000
+    $deploymentName = "cosmosdb" + $suffix
+}
+
+New-AzResourceGroupDeployment -Name $deploymentName `
+-ResourceGroupName $ResourceGroup.ResourceGroupName `
+-TemplateFile $template `
+-TemplateParameterFile $parameters `
+-dbName $deploymentName
